5b14fad0c3388eb42440bdd635f589c1d34e6d2e
RemKouhai <iloveannie2325@gmail.com>
2024-07-06 07:14:49 +0700

    thêm lớp lsd

---
+++ GameProject/MatchHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    public class MatchHistory
+    {
+        public MatchHistory() { }
+        public string RoomName { get; set; }
+        public string Data { get; set; }
+    }
+}
